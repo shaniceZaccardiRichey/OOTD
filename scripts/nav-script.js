8d0e3732137d5b47c428a3c81d46106d18ce6a05
Shanice A Zaccardi-Richey <shanice.zaccardi.richey@gmail.com>
2021-03-09 13:46:43 -0600

    Added font and color styling

--- scripts/nav-script.js
+++ scripts/nav-script.js
@@ -1,19 +1,24 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+var toggleButton = document.querySelector('.toggle-button');
+var navbarLinks = document.querySelector('.navbar-links');
 
 toggleButton.addEventListener('click', () => {
-	navbarLinks.classList.toggle('active')
-})
+	navbarLinks.classList.toggle('active');
+});
 
 
-var modalBtn = document.querySelector('.modal-btn')
-var modalBg = document.querySelector('.modal-bg')
-var modalClose = document.querySelector('.modal-close')
+var modalBtn = document.querySelector('.modal-btn');
+var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
 
-modalBtn.addEventListener('click', function(){
-	modalBg.classList.add('bg-active')
-})
+if(modalBtn) {
+	modalBtn.addEventListener('click', function(){
+		modalBg.classList.add('bg-active');
+	});
+}
 
-modalClose.addEventListener('click', function(){
-	modalBg.classList.remove('bg-active')
-})
+if(modalClose) {
+	modalClose.addEventListener('click', function(){
+		modalBg.classList.remove('bg-active');
+	});
+}
+
