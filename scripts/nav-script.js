b39246b20c3155a7fea5d9708fe795f717efb6dd
Shanice A Zaccardi-Richey <shanice.zaccardi.richey@gmail.com>
2021-03-09 09:17:23 -0600

    Added sizing modal

--- scripts/nav-script.js
+++ scripts/nav-script.js
@@ -3,4 +3,17 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active')
-})// JavaScript Document
+})
+
+
+var modalBtn = document.querySelector('.modal-btn')
+var modalBg = document.querySelector('.modal-bg')
+var modalClose = document.querySelector('.modal-close')
+
+modalBtn.addEventListener('click', function(){
+	modalBg.classList.add('bg-active')
+})
+
+modalClose.addEventListener('click', function(){
+	modalBg.classList.remove('bg-active')
+})
